103d79a3c97635c2be80deda140c24bc387e0ba3
Les Orchard <me@lmorchard.com>
2013-06-13 13:21:38 -0400

    Move manage.py into teamwork_example to de-clutter package root

--- teamwork_example/manage.py
+++ teamwork_example/manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os
+import os.path
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))  
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teamwork_example.settings")
 
 if __name__ == "__main__":
