aecb08f728a717ea817ecd41dffa1b7273f50a85
Les Orchard <me@lmorchard.com>
2015-02-09 17:33:29 -0500

    Upgrade dependencies to Django 1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-teamwork',
-    version='0.0.2',
+    version='0.1.0',
     description='Django app that offers an authorization backend with support for per-object permissions based on combinations of Teams, Roles, and Policies.',
     long_description=open('README.rst').read(),
     author='Les Orchard',
